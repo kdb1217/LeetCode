323fa1427d5ea14e7be3684fb7d718ab332fecf2
Rash <84852135+kdb1217@users.noreply.github.com>
2024-11-18 15:01:52 +0900

    Time: 135 ms (23.21%), Space: 26.2 MB (85.16%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -0,0 +1,18 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # 완탐 x 10 ^ 10이기 때문에 너무 오래걸림 
+        stack = []
+        result = [0] * len(temperatures)
+        print(result)
+        for i in range(len(temperatures) - 1, -1, -1):
+            while stack and temperatures[i] >= temperatures[stack[-1]]:
+                stack.pop()
+
+            if stack:
+                result[i] = stack[-1] - i
+
+            stack.append(i)
+
+        return result
+
+                    
