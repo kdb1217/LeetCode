c689912c56fee12840b278045d98c5198ce8d10d
Rash <84852135+kdb1217@users.noreply.github.com>
2024-11-29 23:03:34 +0900

    Time: 42 ms (8.26%), Space: 17.3 MB (5.21%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        answer = []
+        answerSet = set()
+        candidates.sort()
+        visited = [False] * len(candidates)
+
+        def backtracking(arr, sumArr, idx):
+            if sumArr == target:
+                answer.append(arr)
+                return
+
+            if sumArr > target:
+                return
+
+            for i in range(idx, len(candidates)):
+                if not visited[i]:
+                    visited[i] = True
+                    if i > 0 and candidates[i] == candidates[i - 1] and not visited[i - 1] and candidates[i] <= target:
+                        visited[i] = False
+                        continue
+                    backtracking(arr + [candidates[i]], sumArr + candidates[i], i)
+                    visited[i] = False
+        backtracking([],0,0)
+        return answer
+
+        
